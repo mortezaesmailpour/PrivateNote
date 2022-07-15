f3ab0a1f8e9953bdab3147494161e9de5e0b1a18
Morteza <morteza@esmailpour.net>
2022-07-15 18:10:59 +0430

    add E2E.ApiLayer Test project

---
+++ Tests/PrivateNote.Tests.E2E.ApiLayer/AuthTest.cs
@@ -0,0 +1,29 @@
+namespace PrivateNote.Tests.E2E.ApiLayer;
+public class AuthTests
+{
+    private readonly PrivateNoteClient _sut;
+
+    public AuthTests() => _sut = new PrivateNoteClient();
+
+    [Theory]
+    [InlineData("Alice", "123")]
+    [InlineData("Bob", "Aa@12345")]
+    //[InlineData("User@example.com", "Aa@12345")]
+    public async Task SignIn_ReturnValidToken_WhenUsernameAndPasswordAreCorrect(string username, string password)
+    {
+        // Arrange
+
+        // Act
+        var token = await _sut.SignInAsync(username, password);
+
+        // Assert
+        token.Should().NotBeNull();
+
+        // Act
+        var userInfo = await _sut.WhoAmI(token!);
+
+        // Assert
+        userInfo.Should().NotBeNull();
+        userInfo?.UserName.Should().Be(username);
+    }
+}
---
+++ Tests/PrivateNote.Tests.E2E.ApiLayer/PrivateNoteClient.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using PrivateNote.Api.Dto;
+using PrivateNote.Api.Dto.Responses;
+using PrivateNote.Service.Contract;
+using System.Text;
+
+namespace PrivateNote.Tests.E2E.ApiLayer;
+internal class PrivateNoteClient
+{
+    private readonly string _baseUrl;
+    public PrivateNoteClient()
+    {
+        _baseUrl = "http://localhost/api/Auth/";
+    }
+
+    public async Task<bool> SignUpAsync(string userName, string password)
+    {
+        var signUpData = new Dictionary<string, string>();
+        signUpData.Add("UserName", userName);
+        signUpData.Add("Password", password);
+        HttpClient client = new();
+        var json = JsonConvert.SerializeObject(signUpData);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+        var response = await client.PostAsync(_baseUrl + "SignUp", content);
+        //if (!response.IsSuccessStatusCode)
+        //    _logger.LogError("SignUp Not Success response : " + response.ToString());
+        return response.IsSuccessStatusCode;
+    }
+
+    public async Task<string?> SignInAsync(string userName, string password)
+    {
+        var signInData = new Dictionary<string, string>();
+        signInData.Add("UserName", userName);
+        signInData.Add("Password", password);
+        HttpClient client = new();
+        var json = JsonConvert.SerializeObject(signInData);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+        var response = await client.PostAsync(_baseUrl + "SignIn", content);
+        //if (!response.IsSuccessStatusCode)
+        //    _logger.LogError("Incorrect Username or Password");
+        string responseString = await response.Content.ReadAsStringAsync();
+        var signInResponse = JsonConvert.DeserializeObject<SignInResponse>(responseString);
+        //if (signInResponse is null)
+        //    _logger.LogError("Incorrect Json format in SignIn response");
+        return signInResponse?.Token;
+    }
+
+    public async Task<IUser?> WhoAmI(string token)
+    {
+        HttpClient client = new();
+        client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+        var response = await client.GetAsync(_baseUrl + "HowAmI");
+        //if (!response.IsSuccessStatusCode)
+        //    _logger.LogError("token is not valid");
+        string responseString = await response.Content.ReadAsStringAsync();
+        var userInfo = JsonConvert.DeserializeObject<UserInfo>(responseString);
+        //if (userInfo is null)
+        //    _logger.LogError("Incorrect Json format in SignIn response");
+        return userInfo;
+    }
+}
